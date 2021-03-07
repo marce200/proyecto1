ed307ef8438d3c0b85e7b147f7fd4107122eed37
Usuroot2 Name <usuroot2@example.com>
2021-03-07 03:34:56 +0100

    Añado Cambio 2 desde proyecto2

--- prueba1/src/cambio1/Cambio1.java
+++ prueba1/src/cambio1/Cambio1.java
@@ -3,4 +3,9 @@ public class Cambio1 {
 	public static void main(String[] args) {
 		System.out.println("Pruebas Git. Clase Cambio1");
 	}
+public class Cambio2 {
+	public static void main(String[] args) {
+			System.out.println("Clase Cambio2 en proyecto2");
+			System.out.println("Clase Cambio2 en proyecto2");
+		}	
 }
